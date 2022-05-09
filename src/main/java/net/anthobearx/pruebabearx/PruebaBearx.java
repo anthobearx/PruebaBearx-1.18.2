0204ce4b8eb5a19c57c007068229e6b3e930fc9a
BuildTools <unconfigured@null.spigotmc.org>
2022-05-08 23:41:19 -0600

    New plush! -Shark -Akita -Duck -Josu furry (OC by latory27 on ig)
    New langs
    -English US
    -Spanish MX
    
    -BUG FIXES 1

--- src/main/java/net/anthobearx/pruebabearx/PruebaBearx.java
+++ src/main/java/net/anthobearx/pruebabearx/PruebaBearx.java
@@ -1,16 +1,17 @@
 package net.anthobearx.pruebabearx;
 
+import net.anthobearx.pruebabearx.itembearx.ItemBearx;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PruebaBearx implements ModInitializer {
 
-	public static final Logger LOGGER = LoggerFactory.getLogger("pruebabearx");
+	public static final String MOD_ID = "pruebabearx";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		//idk bro
-		LOGGER.info("Hello Fabric world!");
+		ItemBearx.registerModItems();
 	}
 }
---
+++ src/main/java/net/anthobearx/pruebabearx/itembearx/ItemBearx.java
@@ -0,0 +1,29 @@
+package net.anthobearx.pruebabearx.itembearx;
+
+import net.anthobearx.pruebabearx.PruebaBearx;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ItemBearx {
+    public static final Item PLUSH_SHARK = registerItem("plush_shark",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item PLUSH_AKITA = registerItem("plush_akita",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item PLUSH_DUCK = registerItem("plush_duck",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item PLUSH_JOSU = registerItem("plush_josu",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(PruebaBearx.MOD_ID, name), item);
+    }
+    public static void registerModItems(){
+        PruebaBearx.LOGGER.info("Registering Mod Items for " + PruebaBearx.MOD_ID);
+    }
+}
